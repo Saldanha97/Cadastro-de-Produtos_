32b2f2d1f7f4ecb97dc0cc4c76876e5b65237963
emanuel <emanuelsaldanha17@hotmail.com>
2025-11-28 09:45:51 -0300

    novo

---
+++ routes/Publico.js
@@ -0,0 +1,56 @@
+import express, { json } from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+const router = express.Router()
+router.use(express.json())
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+router.post('/cadastro', async (req, res) => {
+
+
+    try {
+        const { email, password } = req.body
+
+        const salt = await bcrypt.genSalt(10)
+        const hashpassword = await bcrypt.hash(password, salt)
+
+        const users = await prisma.user.create({
+            data: {
+                email,
+                password: hashpassword
+            }
+        })
+
+        res.status(201).json(users)
+
+    } catch (err) {
+        res.status(500).json({ messagen: "Erro no Servidor, Tente novamente" })
+
+    }
+
+})
+
+
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) return res.status(401).json({ message: "Email ou senha incorretos" });
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) return res.status(401).json({ message: "Email ou senha incorretos" });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+
+    res.json({ accessToken: token });
+
+});
+
+export default router
---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import Publico from './routes/Publico.js';
+import Privadas from './routes/privadas.js';
+import auth from './middlewares/auth.js';
+
+const app = express();
+
+app.use(cors({
+    origin: [
+        "https://cadastro-de-produtos-git-master-saldanha97s-projects.vercel.app",
+        "https://cadastro-de-produtos-5esda07g4-saldanha97s-projects.vercel.app",
+        "https://cadastro-de-produtos-sable.vercel.app",
+        "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use('/', Publico)
+app.use('/', auth, list)
+
+
+app.use('/', Publico);
+
+
+app.use('/', auth, Privadas);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor rodando 🚀");
+});
